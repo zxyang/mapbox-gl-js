5616c24f71b9e448570359db5c73fe35b42ffebf
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-03-20 14:23:27 -0400

    fix degenerating lines in outline shader

--- shaders/outline.vertex.glsl
+++ shaders/outline.vertex.glsl
@@ -9,9 +9,9 @@ varying vec2 v_pos;
 void main() {
     // If the x coordinate is the maximum integer, we move the z coordinates out
     // of the view plane so that the triangle gets clipped. This makes it easier
-    // for us to create degenerate triangle strips.
+    // for us to create degenerate lines.
     //float z = step(32767.0, a_pos.x);
     gl_Position = u_posmatrix * vec4(a_pos, 0, 1);
-    gl_Position.z = step(32767.0, a_pos.x);
+    gl_Position.z = (1.0 + step(32767.0, a_pos.x)) * gl_Position.w;
     v_pos = (gl_Position.xy/gl_Position.w + 1.0) / 2.0 * u_world;
 }
