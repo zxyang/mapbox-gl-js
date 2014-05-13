46c5880077f666ea73e4e6c8122c486948c65948
Vladimir Agafonkin <agafonkin@gmail.com>
2014-05-13 18:24:21 +0300

    better rtree viz

--- debug/viz.js
+++ debug/viz.js
@@ -3,17 +3,23 @@ var W = document.body.offsetWidth - 20,
     ctx = canvas.getContext('2d');
 
 canvas.style.width = canvas.style.height = W + 'px';
-canvas.width = canvas.height = W;
+
+var retina = devicePixelRatio > 1;
+
+canvas.width = canvas.height = retina ? W * 2 : W;
+if (retina) {
+    ctx.scale(2, 2);
+}
 
 var colors = ['#f40', '#37f', '#0b0'],
     rects;
 
-function drawTree(node, level) {
+function drawTree(node, level, height) {
     if (!node) { return; }
 
     var rect = [];
 
-    rect.push(level ? colors[(level - 1) % colors.length] : 'grey');
+    rect.push(level ? colors[(height - level - 1) % colors.length] : 'grey');
     // rect.push(level ? 1 / level : 1);
     rect.push(node.leaf ? 1 : 0.3);
     rect.push(node.bbox);
@@ -24,14 +30,14 @@ function drawTree(node, level) {
     // if (level === 6) { return; }
 
     for (var i = 0; i < node.children.length; i++) {
-        drawTree(node.children[i], level + 1);
+        drawTree(node.children[i], level + 1, height);
     }
 }
 
 function transformCoords(coords, max) {
     return [
-        Math.round(W * (coords[0] - max[0]) / (max[2] - max[0])) + 0.5,
-        Math.round(W * (coords[1] - max[1]) / (max[3] - max[1])) + 0.5,
+        Math.round(W * (coords[0] - max[0]) / (max[2] - max[0])),
+        Math.round(W * (coords[1] - max[1]) / (max[3] - max[1])),
         Math.round(W * (coords[2] - coords[0]) / (max[2] - max[0])),
         Math.round(W * (coords[3] - coords[1]) / (max[3] - max[1]))
     ];
@@ -40,10 +46,25 @@ function transformCoords(coords, max) {
 function draw(tree, bounds) {
     bounds = bounds || [0, 0, 4096, 4096];
     rects = [];
-    drawTree(tree.data, 0);
+    drawTree(tree.data, 0, tree.data.height);
 
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, W + 1, W + 1);
+
+    // function anim() {
+    //     var rect = rects.pop();
+    //     if (!rect) return;
+
+    //     ctx.strokeStyle = rect[0];
+    //     ctx.globalAlpha = rect[1];
+
+    //     var coords = transformCoords(rect[2], bounds);
+    //     ctx.strokeRect.apply(ctx, coords);
+
+    //     requestAnimationFrame(anim);
+    // }
+
+    // anim();
 
     for (var i = rects.length - 1; i >= 0; i--) {
         ctx.strokeStyle = rects[i][0];
